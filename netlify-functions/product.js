e142a5a356bdc1174ec7bb16e7e1b70fe3cd76a1
rjmtl <rjmtl930191@gmail.com>
2023-08-29 22:31:36 +0530

    fix

--- netlify-functions/product.js
+++ netlify-functions/product.js
@@ -1,6 +1,26 @@
-exports.handler = async function(event, context) { const id = event.path.split('/').pop(); return { statusCode: 200, 
-    headers: {
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE"
-    },
-body: JSON.stringify({ message: `Product with ID: ${id}` }) }; };
+const {MongoClient} = require("mongodb");
+
+const mongoClient = new MongoClient("mongodb+srv://rajatkumar:faTttSCH5YdY2lZg@cluster0.4dl3qti.mongodb.net/")
+const clientPromise = mongoClient.connect();
+
+const handler = async (event) => {
+    try {
+        const id = event.path.split('/').pop(); 
+        const database = (await clientPromise).db('test')
+        const collection = database.collection('products')
+        const results = await collection.find({id:id});
+        return {
+            statusCode: 200,
+            body: JSON.stringify(results),
+            headers: {
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE"
+            },
+        }
+    } catch (error) {
+        console.log(">>>>>>>>>>>>>>>>>>>",error)
+        return {statusCode:500, body: error.toString() };
+    }
+}  
+
+module.exports = { handler }
