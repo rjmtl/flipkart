a61bd84403a352d650f89bebab2bbb2bf8525f0c
rjmtl <rjmtl930191@gmail.com>
2023-08-29 22:21:04 +0530

    fix

--- netlify-functions/products.js
+++ netlify-functions/products.js
@@ -1,54 +1,4 @@
-// import products from '../model/productSchema';
-// import Products from '../model/productSchema';
-
-// exports.handler = async function(event, context) { 
-
-//     const productData = await products.find({})
-//     console.log(productData);
-
-//     return { 
-//     statusCode: 200, 
-//     headers: {
-//         "Access-Control-Allow-Origin": "*",
-//         "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE"
-//     },
-// body: JSON.stringify(productData)}; };
-
-
-
-// import mongoose from 'mongoose';
-// import Products from '../model/productSchema';
-
-// exports.handler = async function(event, context) {
-//     try {
-//         await mongoose.connect("mongodb+srv://rajatkumar:faTttSCH5YdY2lZg@cluster0.4dl3qti.mongodb.net/", {
-//             useNewUrlParser: true,
-//             useUnifiedTopology: true
-//         });
-//         const allProducts = await Products.find({});
-//         console.log(allProducts)
-//         return {
-//             statusCode: 200,
-//             headers: {
-//                 "Access-Control-Allow-Origin": "*",
-//                 "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE"
-//             },
-//             body: JSON.stringify(allProducts) // Return products as a JSON string
-//         };
-//     } catch (error) {
-//         console.log(error)
-//         return {
-//             statusCode: 500,
-//             body: JSON.stringify({ error: "Internal Server Error" })
-//         };
-        
-//     }
-// };
-
-
 const {MongoClient} = require("mongodb");
-const { default: products } = require("../model/productSchema");
-require('dotenv').config();
 
 const mongoClient = new MongoClient("mongodb+srv://rajatkumar:faTttSCH5YdY2lZg@cluster0.4dl3qti.mongodb.net/")
 const clientPromise = mongoClient.connect();
@@ -63,6 +13,7 @@ const handler = async (event) => {
             body: JSON.stringify(results),
         }
     } catch (error) {
+        console.log(">>>>>>>>>>>>>>>>>>>")
         return {statusCode:500, body: error.toString() };
     }
 }  
