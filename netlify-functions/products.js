71ef89e9d1084c3032482460af972ac581730a6a
rjmtl <rjmtl930191@gmail.com>
2023-08-29 16:23:16 +0530

    fix

--- netlify-functions/products.js
+++ netlify-functions/products.js
@@ -1,6 +1,12 @@
-exports.handler = async function(event, context) { return { statusCode: 200, 
+import Product from '../model/productSchema.js';
+
+
+exports.handler = async function(event, context) { 
+    const products = await Product.find({});    
+    return { 
+    statusCode: 200, 
     headers: {
         "Access-Control-Allow-Origin": "*",
         "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE"
     },
-body: JSON.stringify({ message: 'Products endpoint' }) }; };
+body: response.json(products)}; };
