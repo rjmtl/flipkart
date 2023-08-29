631445c00ea944d7616ecc1bee7b9f7fc3d1bba9
rjmtl <rjmtl930191@gmail.com>
2023-08-29 21:35:33 +0530

    fix

--- netlify-functions/products.js
+++ netlify-functions/products.js
@@ -1,43 +1,40 @@
-// import Product from '../model/productSchema.js';
+import Product from '../model/productSchema.js';
 
 
-// exports.handler = async function(event, context) { 
-//     // console.log(event,context)
-//     const products = await Product.find({});  
-//     console.log(products)  
-//     return { 
-//     statusCode: 200, 
-//     headers: {
-//         "Access-Control-Allow-Origin": "*",
-//         "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE"
-//     },
-// body: products}; };
+exports.handler = async function(event, context) { 
+    return { 
+    statusCode: 200, 
+    headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE"
+    },
+body: JSON.stringify("Hello World")}; };
 
-import mongoose from 'mongoose';
-import Products from '../model/productSchema';
+// import mongoose from 'mongoose';
+// import Products from '../model/productSchema';
 
-exports.handler = async function(event, context) {
-    try {
-        await mongoose.connect("mongodb+srv://rajatkumar:faTttSCH5YdY2lZg@cluster0.4dl3qti.mongodb.net/", {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        });
-        const allProducts = await Products.find({});
-        console.log(allProducts)
-        return {
-            statusCode: 200,
-            headers: {
-                "Access-Control-Allow-Origin": "*",
-                "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE"
-            },
-            body: JSON.stringify(allProducts) // Return products as a JSON string
-        };
-    } catch (error) {
-        console.log(error)
-        return {
-            statusCode: 500,
-            body: JSON.stringify({ error: "Internal Server Error" })
-        };
+// exports.handler = async function(event, context) {
+//     try {
+//         await mongoose.connect("mongodb+srv://rajatkumar:faTttSCH5YdY2lZg@cluster0.4dl3qti.mongodb.net/", {
+//             useNewUrlParser: true,
+//             useUnifiedTopology: true
+//         });
+//         const allProducts = await Products.find({});
+//         console.log(allProducts)
+//         return {
+//             statusCode: 200,
+//             headers: {
+//                 "Access-Control-Allow-Origin": "*",
+//                 "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE"
+//             },
+//             body: JSON.stringify(allProducts) // Return products as a JSON string
+//         };
+//     } catch (error) {
+//         console.log(error)
+//         return {
+//             statusCode: 500,
+//             body: JSON.stringify({ error: "Internal Server Error" })
+//         };
         
-    }
-};
+//     }
+// };
