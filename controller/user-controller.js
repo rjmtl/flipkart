c21d67085b18e36b0b939d29dca5791b7fab819e
rjmtl <rjmtl930191@gmail.com>
2023-08-31 22:41:42 +0530

    fix

--- controller/user-controller.js
+++ controller/user-controller.js
@@ -20,7 +20,7 @@ export const userSignUp = async (request, response) => {
     try {
         const exist = await User.findOne({ username: request.body.username });
         if(exist) {
-            return response.status(401).json({ message: 'User already exist'});
+            return response.status(400).json({ message: 'User already exist'});
         }
         const user = request.body;
         const newUser = new User(user);
